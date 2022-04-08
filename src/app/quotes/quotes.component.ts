f388833a705915c807632a2b8fc7eb3511abc629
Tess <njerigitundu003@gmail.com>
2022-04-08 21:07:58 +0300

    Add quote class and importing the quote class

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes: Quote[] = [
+    new Quote (1, 'People say nothing is impossible, but I do nothing everyday.', 'A.A. Milne', 'Tess', 0, 0)
+  ];
 
   constructor() { }
 
